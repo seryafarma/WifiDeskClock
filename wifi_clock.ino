958159aa8070a717d4aed53db57afc4652f46b86
seryafarma <enhope.estheim@gmail.com>
2023-02-11 13:32:38 +0100

    Update wifi_clock.ino
    Remove unused code.

--- wifi_clock.ino
+++ wifi_clock.ino
@@ -52,26 +52,6 @@ MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 #define SLEEPTIME 2000  // in milliseconds
 #define TIMEZONE "Europe/Amsterdam"
 
-void scrollText(const char* p) {
-    uint8_t charWidth;
-    uint8_t cBuf[8];  // this should be ok for all built-in fonts
-
-    PRINTS("\nScrolling text");
-    mx.clear();
-
-    while (*p != '\0') {
-        charWidth = mx.getChar(*p++, sizeof(cBuf) / sizeof(cBuf[0]), cBuf);
-
-        for (uint8_t i = 0; i <= charWidth;
-             i++)  // allow space between characters
-        {
-            mx.transform(MD_MAX72XX::TSL);
-            if (i < charWidth) mx.setColumn(0, cBuf[i]);
-            delay(DELAYTIME);
-        }
-    }
-}
-
 void rows() {
     PRINTS("\nRows 0->7");
     mx.clear();
